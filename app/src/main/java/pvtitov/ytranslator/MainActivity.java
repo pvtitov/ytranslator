3746f0729011aa771d0a24f2e94dc31f153dc84d
Pavel Titov <pavel_titov@mail.ru>
2017-07-16 14:10:02 +0300

    Fragment arguments

--- app/src/main/java/pvtitov/ytranslator/MainActivity.java
+++ app/src/main/java/pvtitov/ytranslator/MainActivity.java
@@ -6,17 +6,18 @@ import android.support.v4.app.Fragment;
 
 public class MainActivity extends AbstractFragmentActivity {
 
-    public static final String EXTRA_WORD_FROM_HISTORY = "paveltitov.ytranslator.word_to_translate";
+    private static final String EXTRA_WORD = "paveltitov.ytranslator.word";
 
     public static Intent createIntent(Context packageContext, String word){
         Intent intent = new Intent(packageContext, MainActivity.class);
-        intent.putExtra(EXTRA_WORD_FROM_HISTORY, word);
+        intent.putExtra(EXTRA_WORD, word);
         return intent;
     }
 
     @Override
     protected Fragment createRequestFragment() {
-        return new RequestFragment();
+        String word = (String) getIntent().getStringExtra(EXTRA_WORD);
+        return RequestFragment.newInstance(word);
     }
 
     @Override
--- app/src/main/java/pvtitov/ytranslator/RequestFragment.java
+++ app/src/main/java/pvtitov/ytranslator/RequestFragment.java
@@ -9,22 +9,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 /**
  * Created by Павел on 11.06.2017.
  */
 
 public class RequestFragment extends Fragment{
+    private  static final String ARGUMENTS_WORD = "word";
+
     private EditText inputField;
-    private Button button;
+    private ImageButton button;
+
+    public static RequestFragment newInstance(String word){
+        Bundle arguments = new Bundle();
+        arguments.putString(ARGUMENTS_WORD, word);
+        RequestFragment fragment = new RequestFragment();
+        fragment.setArguments(arguments);
+        return fragment;
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_request, container, false);
         inputField = (EditText) view.findViewById(R.id.input_field);
-        inputField.setText(getActivity().getIntent().getStringExtra(MainActivity.EXTRA_WORD_FROM_HISTORY));
-        button = (Button) view.findViewById(R.id.go_button);
+        inputField.setText(getArguments().getString(ARGUMENTS_WORD));
+        button = (ImageButton) view.findViewById(R.id.go_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
