15b8ce15062e33cfe1ecf88144a059415852f7de
Pavel Titov <pavel_titov@mail.ru>
2017-08-13 18:12:55 +0300

    alternative resources - orientation

--- app/src/main/java/pvtitov/ytranslator/ListFragment.java
+++ app/src/main/java/pvtitov/ytranslator/ListFragment.java
@@ -1,8 +1,6 @@
 package pvtitov.ytranslator;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
--- app/src/main/java/pvtitov/ytranslator/MainActivity.java
+++ app/src/main/java/pvtitov/ytranslator/MainActivity.java
@@ -1,7 +1,5 @@
 package pvtitov.ytranslator;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -14,38 +12,14 @@ import android.widget.Toast;
 public class MainActivity extends FragmentActivity
         implements RequestFragment.onTranslationListener, ListFragment.onSelectWordListener{
 
-    private static final String EXTRA_WORD = "paveltitov.ytranslator.word";
-    private static final String EXTRA_TRANSLATION = "paveltitov.ytranslator.translation";
-    private static final String EXTRA_SHOW_DETAIL = "paveltitov.ytranslator.show_detail";
     private static final String REQUEST_FRAGMENT_TAG = "paveltitov.yprompter.request_fragment";
     public static final String LIST_FRAGMENT_TAG = "paveltitov.yprompter.list_fragment";
 
 
-    public static Intent createIntent(Context packageContext, String word, String translation){
-        Intent intent = new Intent(packageContext, MainActivity.class);
-        intent.putExtra(EXTRA_WORD, word);
-        intent.putExtra(EXTRA_TRANSLATION, translation);
-        return intent;
-    }
-
-    public static Intent createIntent(Context packageContext, String word, String translation, boolean showDetail){
-        Intent intent = new Intent(packageContext, MainActivity.class);
-        intent.putExtra(EXTRA_WORD, word);
-        intent.putExtra(EXTRA_TRANSLATION, translation);
-        intent.putExtra(EXTRA_SHOW_DETAIL, showDetail);
-        return intent;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (this.getIntent().getBooleanExtra(EXTRA_SHOW_DETAIL, false)){
-            setContentView(R.layout.container_activity_with_article);
-            TextView textView = findViewById(R.id.article_temporary);
-            textView.setText(this.getIntent().getStringExtra(EXTRA_WORD) + " - " + this.getIntent().getStringExtra(EXTRA_TRANSLATION));
-        } else {
-            setContentView(R.layout.container_activity);
-        }
+        setContentView(R.layout.container_activity);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
@@ -85,6 +59,8 @@ public class MainActivity extends FragmentActivity
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.request_fragment_container, RequestFragment.newInstance(word.getWord()), REQUEST_FRAGMENT_TAG)
+                .addToBackStack(null)
                 .commit();
+        Toast.makeText(this, word.getWord() + " - " + word.getTranslation(), Toast.LENGTH_SHORT).show();
     }
 }
